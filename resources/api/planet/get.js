c826563bb4718656664c20318e7ffad2a11f242f
Kevyn Estrada <kevyn.estrada@qdsgroup.com.pe>
2021-01-18 00:48:09 -0500

    fix result empty

--- resources/api/planet/get.js
+++ resources/api/planet/get.js
@@ -1,36 +1,37 @@
 'use strict';
 
-const https = require('https');
+const axios = require('axios');
 
-module.exports.get = async (event, context, callback) => {
+module.exports.get = (event, context, callback) => {
 
-  let dataString = '';
-    
-    const response = await new Promise((resolve, reject) => {
-      console.log(event.pathParameters.id);
-        const req = https.get("https://swapi.dev/api/planets/" + event.pathParameters.id, function(res) {
-          res.on('data', chunk => {
-            dataString += chunk;
-            console.log(chunk);
-          });
-          res.on('end', () => {
-            resolve({
-                statusCode: 200,
-                body: dataString
-            });
-          });
-        });
-        req.on('error', (e) => {
-          reject({
-              statusCode: 500,
-              body: 'Something went wrong!'
-          });
-        });
-    });
-    console.log(response);
-    callback(null, {
-      statusCode: response.statusCode,
-      headers: { 'Content-Type': 'text/plain' },
-      body: response.body,
+  axios.get("https://swapi.dev/api/planets/" + event.pathParameters.id)
+    .then(response => {
+      console.log(response.data);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          nombre: response.data.name,
+          periodo_rotacion: response.data.rotation_period,
+          periodo_orbital: response.data.orbital_period,
+          diametro: response.data.diameter,
+          clima: response.data.climate,
+          gravedad: response.data.gravity,
+          terreno: response.data.terrain,
+          superficie_Agua: response.data.surface_water,
+          poblacion: response.data.population,
+          residentes: response.data.residents,
+          peliculas: response.data.films,
+          creado: response.data.created,
+          editado: response.data.edited,
+          url: response.data.url,
+        }),
+      })
+    })
+    .catch(error => {
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'El planeta no existe',
+      })
     });
 };
